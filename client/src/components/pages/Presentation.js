6cd19fd4f017fc03758936e36c968bf0851e31d4
Chris-Fortier <vendor@chrisjfortier.com>
2020-07-24 09:01:00 -0700

    presentation update

--- client/src/components/pages/Presentation.js
+++ client/src/components/pages/Presentation.js
@@ -13,8 +13,10 @@ export default function StyleTester() {
                   <li>https://loadoutapp.herokuapp.com/</li>
                   <li>
                      View the account settings page so the background image is
-                     loaded View the console (so that we don’t have blue
-                     overlays randomly appearing)
+                     loaded View the console{" "}
+                     <span className="presentation-action">
+                        so that we don’t have blue overlays randomly appearing
+                     </span>
                   </li>
                   <li>
                      Inpsect, change to console, undock it and move it to second
@@ -46,7 +48,9 @@ export default function StyleTester() {
                      totally free.
                   </li>
                   <li>My capstone project is an app called Loadout.</li>
-                  <li>(Landing page)</li>
+                  <li>
+                     <span className="presentation-action">Landing page</span>
+                  </li>
                   <ul>
                      <li>
                         One thing that I always find frustrating is packing for
@@ -63,41 +67,63 @@ export default function StyleTester() {
                      </li>
                      <li>
                         From the landing page, you can log in or sign up to make
-                        a new account (show the sign up page, then go back to
-                        log in)
+                        a new account{" "}
+                        <span className="presentation-action">
+                           show the sign up page, then go back to log in
+                        </span>
                      </li>
                   </ul>
-                  <li>(Log on using chris.)</li>
+                  <li>
+                     <span className="presentation-action">
+                        Log on using chris.
+                     </span>
+                  </li>
                   <ul>
                      <li>
-                        (Scroll the list on My Loadouts.) When you log in you
-                        will a see a list of all your loadouts, as well as
-                        loadouts that other users have shared with you.
+                        <span className="presentation-action">
+                           Scroll the list on My Loadouts.
+                        </span>{" "}
+                        When you log in you will a see a list of all your
+                        loadouts, as well as loadouts that other users have
+                        shared with you.
                      </li>
                   </ul>
-                  <li>(Click One-Night Camping Trip.)</li>
+                  <li>
+                     <span className="presentation-action">
+                        Click One-Night Camping Trip.
+                     </span>
+                  </li>
                   <ul>
                      <li>
-                        Here is my loadout for a one-night camping trip. (Scroll
-                        the loadout.)
+                        Here is my loadout for a one-night camping trip.{" "}
+                        <span className="presentation-action">
+                           Scroll the loadout.
+                        </span>
                      </li>
                      <li>
                         This app is responsive. When you turn your phone
                         sideways, you will see two columns of items
-                        (demonstrate).
+                        <span className="presentation-action">demonstrate</span>
+                        .
                      </li>
                      <li>
-                        You can also use your computer. (Change to computer
-                        mode.)
+                        You can also use your computer.{" "}
+                        <span className="presentation-action">
+                           Change to computer mode.
+                        </span>
                      </li>
                      <li>
-                        (Move the cursor over the items.) These cards are React
-                        components, each one representing an item in this
-                        loadout.
+                        <span className="presentation-action">
+                           Move the cursor over the items.
+                        </span>{" "}
+                        These cards are React components, each one representing
+                        an item in this loadout.
                      </li>
                      <li>
-                        To pack an item, click its icon to toggle it. (Pack
-                        everything you can on this page.)
+                        To pack an item, click its icon to toggle it.{" "}
+                        <span className="presentation-action">
+                           Pack everything you can on this page.
+                        </span>
                      </li>
                      <li>
                         This is a full stack application. Any changes you make
@@ -107,83 +133,149 @@ export default function StyleTester() {
                         would see me packing these items.
                      </li>
                      <li>
-                        You will notice that some items cannot be packed (hover
-                        over the Day Pack and Camera Bag pack icons). This is
-                        because they contain sub items that need to be packed
-                        first. (Hover over the subitem indicators.) You cannot
-                        pack an item until you have packed everything inside of
-                        it.
+                        You will notice that some items cannot be packed{" "}
+                        <span className="presentation-action">
+                           hover over the Day Pack and Camera Bag pack icons
+                        </span>
+                        . This is because they contain sub items that need to be
+                        packed first.{" "}
+                        <span className="presentation-action">
+                           Hover over the subitem indicators.
+                        </span>{" "}
+                        You cannot pack an item until you have packed everything
+                        inside of it.
                      </li>
                   </ul>
-                  <li>(Open the Day Pack.)</li>
+                  <li>
+                     <span className="presentation-action">
+                        Open the Day Pack.
+                     </span>
+                  </li>
                   <ul>
                      <li>
                         When you open the day pack you can see the four items
-                        that need to be packed. (Pack the three that you can,
-                        then hover over the First Aid kit packed indicator.)
+                        that need to be packed.{" "}
+                        <span className="presentation-action">
+                           Pack the three that you can, then hover over the
+                           First Aid kit packed indicator.
+                        </span>
                      </li>
                      <li>
                         You cannot pack the first aid kit because it also
                         contains unpacked subitems.
                      </li>
-                     <li>(Open the First Aid Kit.)</li>
+                     <li>
+                        <span className="presentation-action">
+                           Open the First Aid Kit.
+                        </span>
+                     </li>
                      <ul>
                         <li>
                            You can create a loadout that is several levels deep.
                         </li>
-                        <li>(Pack the two items, then go back to Day Pack.)</li>
+                        <li>
+                           <span className="presentation-action">
+                              Pack the two items, then go back to Day Pack.
+                           </span>
+                        </li>
                      </ul>
                      <li>
-                        Now the first aid kit is ready to be packed (pack it).
+                        Now the first aid kit is ready to be packed{" "}
+                        <span className="presentation-action">pack it</span>.
                      </li>
                   </ul>
-                  <li>(Go up to One-Night Camping Trip.)</li>
+                  <li>
+                     <span className="presentation-action">
+                        Go up to One-Night Camping Trip.
+                     </span>
+                  </li>
                   <ul>
-                     <li>Now you can finally pack the Day pack (pack it).</li>
+                     <li>
+                        Now you can finally pack the Day pack{" "}
+                        <span className="presentation-action">pack it</span>.
+                     </li>
                   </ul>
-                  <li>If you click here you will enter Edit Mode (do it).</li>
+                  <li>
+                     If you click here you will enter Edit Mode{" "}
+                     <span className="presentation-action">do it</span>.
+                  </li>
                   <ul>
                      <li>
-                        In Edit Mode, you can add items and subitems (click to
-                        add an item.),
+                        In Edit Mode, you can add items and subitems{" "}
+                        <span className="presentation-action">
+                           click to add an item.
+                        </span>
+                        ,
                      </li>
                      <li>
-                        name or rename them (name it “snacks for the road”),
+                        name or rename them{" "}
+                        <span className="presentation-action">
+                           name it “snacks for the road”
+                        </span>
+                        ,
                      </li>
-                     <li>and delete items (delete the item you just made).</li>
+                     <li>
+                        and delete items{" "}
+                        <span className="presentation-action">
+                           delete the item you just made
+                        </span>
+                        .
+                     </li>
                      <li>
                         If you try to delete something that contains subitems
-                        (click the X next to Cooler), it will give you a warning
-                        saying how many subitems would also get deleted (click
-                        Cancel).
+                        <span className="presentation-action">
+                           click the X next to Cooler
+                        </span>
+                        , it will give you a warning saying how many subitems
+                        would also get deleted{" "}
+                        <span className="presentation-action">
+                           click Cancel
+                        </span>
+                        .
                      </li>
                   </ul>
-                  <li>(Click Loadout Settings.)</li>
+                  <li>
+                     <span className="presentation-action">
+                        Click Loadout Settings.
+                     </span>
+                  </li>
                   <ul>
                      <li>
                         On this page you can share your loadout with other
                         users.
                      </li>
                      <li>
-                        Once you enter a valid username (add a user), you can
-                        assign them different permissions, depending on what you
-                        want them to be able to do, such as being able to pack
-                        and unpack; edit the loadout; or have admin privileges.
-                        (Uncheck the first two, save changes, then click the
-                        remove button to remove the new user.)
+                        Once you enter a valid username{" "}
+                        <span className="presentation-action">add a user</span>,
+                        you can assign them different permissions, depending on
+                        what you want them to be able to do, such as being able
+                        to pack and unpack; edit the loadout; or have admin
+                        privileges.
+                        <span className="presentation-action">
+                           Uncheck the first two, save changes, then click the
+                           remove button to remove the new user.
+                        </span>
                      </li>
                   </ul>
                   <li>
-                     (Click the upper right menu and go to Account Settings.)
+                     <span className="presentation-action">
+                        Click the upper right menu and go to Account Settings.
+                     </span>
                   </li>
                   <ul>
                      <li>
                         In account settings, you can change your username,
-                        password or delete your account. (Click to show each of
-                        the submenus.)
+                        password or delete your account.{" "}
+                        <span className="presentation-action">
+                           Click to show each of the submenus.
+                        </span>
                      </li>
                   </ul>
-                  <li>(Click to log out)</li>
+                  <li>
+                     <span className="presentation-action">
+                        Click to log out
+                     </span>
+                  </li>
                   <ul>
                      <li>And that is Loadout</li>
                      <li>
